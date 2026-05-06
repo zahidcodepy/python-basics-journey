86846f14c23d2bac2a4d9d94985b1e58c61bf4a9
Zahid Khan <zahidkhan18100@gmail.com>
2026-05-06 22:46:03 +0530

    Create userinput.py for handling user input
    Added a script to take user input and demonstrate type conversion.

---
+++ Basics/userinput.py
@@ -0,0 +1,17 @@
+# Taking user input in python
+# Input Function
+name = input("Enter your name:")
+# keypoints : 1.Always returns String
+print("My Name is", name)
+
+# For identifying TYPE
+# Example
+x = input("enter number:")
+print(type(x))
+
+# Type conversion
+age = int(input("Enter your Age:"))
+# OUTPUT should be integer
+"""python converts i/p only when we tell it"""
+# No automatic type detection
+# fully control by us only
