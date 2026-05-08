fc85761f30c45530da90bf7f5caade5416f30440
Zahid Khan <zahidkhan18100@gmail.com>
2026-05-09 01:05:43 +0530

    Enhance stringmethod.py with additional string methods
    Added examples of rstrip, replace, and split methods.

--- Basics/stringmethod.py
+++ Basics/stringmethod.py
@@ -1,7 +1,10 @@
 # String Method
 # IMPORTANT - STRINGS ARE IMMUTABLE
 # Python provides a set of BUILT-IN-Methods that we can use to alter and modify the strings
-a = "Khan"
+a = "!!!Khan!! !!!!!!!! Khan!!"
 print(len(a))
 print(a.upper())  # upper method in string
 print(a.lower())  # lower method in string
+print(a.rstrip("!")) # rstrip method in string( Removes trailing characters from the right side of the string. and removes specified characters from the end of the string. If no argument is passed, whitespace is removed.
+print(a.replace("Khan", "Zahid")) # it will replace string 1 to another.
+print(a.split(" ")) # Splits a string into a list.
