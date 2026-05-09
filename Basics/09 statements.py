2e2f3820e04fc3fb2ab562c969471e2d84302f93
Zahid Khan <zahidkhan18100@gmail.com>
2026-05-09 23:23:46 +0530

    Implement examples of conditional statements in Python
    Added examples of if, elif, and nested if statements in Python.

---
+++ Basics/09 statements.py
@@ -0,0 +1,30 @@
+# IF ELSE STATEMENT
+# Used for decision making
+# EXAMPLE
+age = 18
+if age >= 18:
+    print("You can vote")
+else:
+    print("You can not vote")
+
+# IF ELIF ELSE STATEMENT
+# USED WHEN MULTIPLE CONDITION EXIST
+# EXAMPLE
+MARKS = 88
+if MARKS>=90:
+    print("Excellent")  
+elif MARKS>=70:
+    print("Good") 
+elif MARKS>=50:
+    print("Average")      
+else:
+    print("Fail")
+
+# Nested If Statement
+# we can use if,if-else,elif statement inside other if statement as well
+age = 20
+citizen = True
+if age >= 18:
+    if citizen:
+        print("Eligible to vote")
+        
