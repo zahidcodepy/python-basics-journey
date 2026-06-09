4c8345109824dd4a013566bf69519aaf507b71de
Zahid Khan <zahidkhan18100@gmail.com>
2026-06-09 22:21:57 +0530

    Add match-case example for day selection
    Added example of using match-case for day selection.

--- Basics/11 matchcase.py
+++ Basics/11 matchcase.py
@@ -10,3 +10,18 @@ match variable:
         # # match checks the value of a variable.
 # case defines possible matches.
 # ("_") acts as the default case (similar to else)
+
+# EXAMPLE
+day = 3
+
+match day:
+    case 1:
+        print("Monday")
+    case 2:
+        print("Tuesday")
+    case 3:
+        print("Wednesday")
+    case _:
+        print("Invalid Day")
+
+# OUTPUT- Wednesday
