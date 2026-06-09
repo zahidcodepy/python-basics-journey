6d788fe97e42feed2fb2f26592208d23e8fe36b4
Zahid Khan <zahidkhan18100@gmail.com>
2026-06-09 22:07:20 +0530

    Document match-case syntax in matchcase.py
    Added comments explaining the syntax and usage of match-case statements in Python.

--- Basics/11 matchcase.py
+++ Basics/11 matchcase.py
@@ -1,1 +1,12 @@
-
+# SYNTAX for Match-Case Statements in Python
+match variable:
+    case value1:
+        # Code to execute if variable matches value1
+    case value2:
+        # Code to execute if variable matches value2
+    case _:
+        # Code to execute if variable does not match any of the above cases
+        # Default case (optional)
+        # # match checks the value of a variable.
+# case defines possible matches.
+# _ acts as the default case (similar to else)
