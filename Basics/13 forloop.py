753ffee55c29c7694b858ccae7e77c461f9ef0a4
Zahid Khan <zahidkhan18100@gmail.com>
2026-06-10 19:31:55 +0530

    Refine comments in for loop example
    Updated comments to clarify the for loop example output.

--- Basics/13 forloop.py
+++ Basics/13 forloop.py
@@ -6,4 +6,12 @@
 language = "PYTHON"
 for i in language:
     print(i)
-# In this example, we have a string "PYTHON". The for loop iterates over each character in the string and prints it. The variable 'i' takes on the value of each character in the string during each iteration of the loop.
+# In this example, we have a string "PYTHON". The for loop iterates over each character in the string and prints it.
+# The variable 'i' takes on the value of each character in the string during each iteration of the loop.
+# The output of the above code will be:
+# P
+# Y
+# T
+# H
+# O
+# N
