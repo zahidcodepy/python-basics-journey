f334b50e56176290fcef886b11d954b2d4be31f7
Zahid Khan <zahidkhan18100@gmail.com>
2026-06-10 12:45:09 +0530

    Add documentation for loops in Python
    Added comments explaining loops in Python.

--- Basics/12 loops.py
+++ Basics/12 loops.py
@@ -1,1 +1,6 @@
-
+# Loops in Python.
+# Loops enable you to perform repetitive tasks efficiently without writing redundant code. They iterate over a sequence (like a list, tuple, string, or range) or execute a block of code as long as a specific condition is met.
+# Types of Loops in Python
+# 1. While loop
+# 2. For loop
+# 3. Nested loop
