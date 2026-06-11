c38f68889f3f0571bb533a9937d201e3921c58c7
Zahid Khan <zahidkhan18100@gmail.com>
2026-06-11 21:02:29 +0530

    Add examples for range function in for loops
    Added examples demonstrating the usage of the range function in for loops.

--- Basics/13 forloop.py
+++ Basics/13 forloop.py
@@ -25,3 +25,11 @@ for i in language:
 # range(start, stop)
 # range(start, stop, step)
 
+# Example1: Basic usage with One Argument - Stop
+for x in range(5):
+    print(x)
+# Output: 0 1 2 3 4
+# Example2: Basic usage with Start, Stop and Step
+for y in range(1, 10, 2):
+    print(y)
+# Output: 1 3 5 7 9
