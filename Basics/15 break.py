f80ce3a9d08e392ff643965ba5286df3e300d526
Zahid Khan <zahidkhan18100@gmail.com>
2026-06-14 02:06:17 +0530

    Document break statement usage in Python
    Added comments explaining the break statement in Python loops with an example.

--- Basics/15 break.py
+++ Basics/15 break.py
@@ -1,1 +1,27 @@
 
+# break Statement: The break statement terminates the loop entirely, exiting from it immediately.
+# It is commonly used to exit a loop when a certain condition is met, preventing further iterations.
+
+# SYNTAX
+# while condition:
+    # code block to be executed
+    # if some_condition:
+        # break 
+
+# EXAMPLE for break statement in a for loop
+i = 0
+for i in range(5):
+     print(i)
+     if i == 3:
+        break
+
+# output:
+# 0
+# 1
+# 2
+# 3
+# In this example, the for loop iterates over the range of numbers from 0 to 4.
+# When the variable 'i' reaches the value of 3, the break statement is executed, which causes the loop to terminate immediately.
+# as a result, the loop will not count beyond 3.
+
+
