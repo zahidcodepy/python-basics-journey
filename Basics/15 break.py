a9a611e9c51a86b9386ef22ce95f9e7e54b6eae6
Zahid Khan <zahidkhan18100@gmail.com>
2026-06-14 02:06:44 +0530

    Add while loop example for break statement
    Added an example of the break statement in a while loop.

--- Basics/15 break.py
+++ Basics/15 break.py
@@ -24,4 +24,21 @@ for i in range(5):
 # When the variable 'i' reaches the value of 3, the break statement is executed, which causes the loop to terminate immediately.
 # as a result, the loop will not count beyond 3.
 
+# EXAMPLE for break statement in a while loop
 
+count = 0 
+while count < 10:
+    print(count)
+    if count == 5:
+        break
+    count = count + 1
+# output:
+# 0
+# 1
+# 2
+# 3
+# 4
+# 5
+# In this example, the while loop continues to execute as long as the value of 'count' is less than 10.
+# When 'count' reaches the value of 5, the break statement is executed, which causes the loop to terminate immediately.
+# As a result, the loop will not count beyond 5.
