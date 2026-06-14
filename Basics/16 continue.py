2ff6e78e05005ab7a12a42d092e6d50bcd1487eb
Zahid Khan <zahidkhan18100@gmail.com>
2026-06-14 23:55:58 +0530

    Document continue statement in Python
    Added explanation and example for the continue statement.

--- Basics/16 continue.py
+++ Basics/16 continue.py
@@ -1,1 +1,17 @@
+# CONTINUE STATEMENT
+# The continue statement is used to skip the current iteration of a loop and move to the next iteration.
+
+# SYNTAX
+
+# for/while condition:
+#     if condition:
+#          continue
+
+
+# Example
+
+for i in range(1,8):
+    if i == 3:
+        continue
+print(i)
 
