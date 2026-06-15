d7a25dfbe2a898f1660047c4bb55bf64896ab482
Zahid Khan <zahidkhan18100@gmail.com>
2026-06-16 01:36:50 +0530

    Add function definition syntax example

--- Basics/17 functions.py
+++ Basics/17 functions.py
@@ -6,3 +6,9 @@
 # • Call function: Use the function's name followed by () to run it.
 # • Parameter: The variable listed inside parentheses in function definition.
 # • Argument: The actual value you pass to function when you call it.
+
+# SYNTAX
+
+# def function_name(parameters):
+    # code block to be executed
+    # return value (optional)
