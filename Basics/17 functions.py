c898132ff69f7aeabde58943dd0c9378a70ceb6e
Zahid Khan <zahidkhan18100@gmail.com>
2026-06-17 03:37:54 +0530

    Implement greet function for personalized greeting
    Added a greet function that prints a greeting message and returns a confirmation string.

--- Basics/17 functions.py
+++ Basics/17 functions.py
@@ -12,3 +12,16 @@
 # def function_name(parameters):
     # code block to be executed
     # return value (optional)
+
+
+# EXAMPLE
+
+def greet(name): # 'name' is a parameter of the function 'greet'
+    print("Hello, " + name +" Welcome to the world of Python!") # this line will print the greeting message
+    return "Greeting sent to " + name  # This line will return a string       
+
+# Calling the function
+result = greet("KHAN")  
+print(result)
+
+# In this example, we define a function called 'greet' that takes one parameter 'name'.
