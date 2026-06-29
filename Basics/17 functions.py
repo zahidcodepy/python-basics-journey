45844321706f64d98f5567870f1ea2ffc0d2d511
Zahid Khan <zahidkhan18100@gmail.com>
2026-06-28 23:50:33 -0700

    Remove unnecessary blank lines in functions.py

--- Basics/17 functions.py
+++ Basics/17 functions.py
@@ -26,8 +26,6 @@ print(result)
 
 # In this example, we define a function called 'greet' that takes one parameter 'name'.
 
-
-
 # Example 2
 
 def find_square(num): # 'num' is a parameter of the function 'find_square'
